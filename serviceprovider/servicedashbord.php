6922c0d496b710e835f6755086a96a38ca548737
lakshitha779988 <lakshitha779988@gmail.com>
2024-11-19 17:52:22 +0530

    remove service status list from slid bar

--- serviceprovider/servicedashbord.php
+++ serviceprovider/servicedashbord.php
@@ -68,7 +68,6 @@ $providerName = mysqli_fetch_assoc($resultProvider)['provider_name'] ?? "Service
                     <li><a href="servicedashbord.php">Home</a></li>
                     <li><a href="servicelisting.php">Add service</a></li>
                     <li><a href="#" onclick="loadContent('all_service')">All service</a></li>
-                    <li><a href="#" onclick="loadContent('product_status')">Product Status</a></li>
                     <li><a href="#" onclick="loadContent('service_request')">Ordered Products</a></li>
                     <li><a href="../Home/dashbord.php">Back to Home</a></li>
                 </ul>
